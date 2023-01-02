12c5dfba3a0b5b91d8553ba7dc7399c6593af930
pulkitupadhyay16 <pulkitu71@gmail.com>
2023-01-02 14:23:23 +0530

    first question

--- RevesePolish.java
+++ RevesePolish.java
@@ -1,3 +1,30 @@
-class ReversePolish{
-    
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<String> st =new Stack<>();
+        for(String op : tokens){
+            if(op.equals("+")){
+                int a =Integer.parseInt(st.pop());
+                int b=Integer.parseInt(st.pop());
+                st.push(String.valueOf(a+b));
+            }else if(op.equals("-")){
+                int b =Integer.parseInt(st.pop());
+                int a=Integer.parseInt(st.pop());
+                st.push(String.valueOf(a-b));
+
+            }else if(op.equals("*")){
+                int a =Integer.parseInt(st.pop());
+                int b=Integer.parseInt(st.pop());
+                st.push(String.valueOf(a*b));
+            }else if(op.equals("/")){
+                int b =Integer.parseInt(st.pop());
+                int a=Integer.parseInt(st.pop());
+                st.push(String.valueOf(a/b));
+
+
+            }else{
+                st.push(op);
+            }
+        }
+        return Integer.parseInt(st.pop());
+    }
 }
